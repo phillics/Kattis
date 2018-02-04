7f7654a34fbd88475b713400dbeca500337da274
Spencer Phillips <36007912+phillics@users.noreply.github.com>
2018-02-04 14:03:36 -0500

    Create Carrots.java

---
+++ Carrots.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Carrots{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int carrot = sc.nextInt();
+        carrot = sc.nextInt();
+        System.out.printf("%d", carrot);
+    }
+}
